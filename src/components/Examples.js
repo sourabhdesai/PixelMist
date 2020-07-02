0190d9a6d7bfe6ee37e8eb290c1bc46f85793b88
sourabhdesai <sourabhdesai@gmail.com>
2020-07-02 00:06:22 -0500

    Fix example images in GH pages site

--- src/components/Examples.js
+++ src/components/Examples.js
@@ -5,12 +5,13 @@ import {adjustDimension} from "../utils/ScreenUtils";
 import imageExamples from './imageExamples.json';
 
 function ImageExample({filename, r, g, b, w, h}) {
+    const baseImagePath = `${(window.location.hostname === 'localhost' ? '' : '/PixelMist')}/images/examples`;
     w = adjustDimension(w);
     h = adjustDimension(h);
     const queryParams = new URLSearchParams({r, g, b, w, h});
     return (
-        <a href={`/PixelMist?${queryParams.toString()}`}>
-            <img alt={filename} id={filename} className="example-img" src={`/images/examples/${filename}`} />
+        <a href={`/PixelMist?${queryParams.toString()}`} key={filename}>
+            <img alt={filename} id={filename} className="example-img" src={`${baseImagePath}/${filename}`} />
         </a>
     );
 }
