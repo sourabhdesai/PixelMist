9fae1fe48a61ee25148e975b90569be7a68433a1
sourabhdesai <sourabhdesai@gmail.com>
2020-07-03 16:51:32 -0500

    fix GA location updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
+  useLocation,
   Switch,
   Route,
   Link,
   Redirect
 } from "react-router-dom";
-import {createBrowserHistory} from 'history';
 import ReactGA from 'react-ga';
 import GitHubButton from 'react-github-btn';
 import './App.css';
@@ -15,39 +14,39 @@ import ExpressionBasedPixelGen from './components/ExpressionBasedPixelGen';
 import About from './components/About';
 import Examples from './components/Examples';
 
-const history = createBrowserHistory();
-history.listen(location => {
-  ReactGA.set({ page: location.pathname })
-  ReactGA.pageview(location.pathname)
-})
+
+function updateLocationForGA(location) {
+  ReactGA.set({ page: location.pathname });
+  ReactGA.pageview(location.pathname);
+}
 
 function App() {
+  const location = useLocation();
+  updateLocationForGA(location);
   return (
     <div className="App">
-      <Router>
-        <Title />
-        <Switch>
-          <Route exact path="/PixelMist">
-            <ExpressionBasedPixelGen />
-          </Route>
-          <Route exact path="/PixelMist/about">
-            <About />
-          </Route>
-          <Route exact path="/PixelMist/examples">
-            <Examples />
-          </Route>
-          <Redirect to="/PixelMist" />
-        </Switch>
-        <div className="footer">
-          <Link to="/PixelMist/about"><h2 className="footer-link">About</h2></Link>
-          <Link to="/PixelMist/examples"><h2 className="footer-link">Examples</h2></Link>
-          <div className="github-btn">
-            <GitHubButton href="https://github.com/sourabhdesai/PixelMist"
-              data-color-scheme="no-preference: dark; light: dark; dark: dark;"
-              data-icon="octicon-star" aria-label="Star sourabhdesai/PixelMist on GitHub">Star</GitHubButton>
-          </div>
+      <Title />
+      <Switch>
+        <Route exact path="/PixelMist">
+          <ExpressionBasedPixelGen />
+        </Route>
+        <Route exact path="/PixelMist/about">
+          <About />
+        </Route>
+        <Route exact path="/PixelMist/examples">
+          <Examples />
+        </Route>
+        <Redirect to="/PixelMist" />
+      </Switch>
+      <div className="footer">
+        <Link to="/PixelMist/about"><h2 className="footer-link">About</h2></Link>
+        <Link to="/PixelMist/examples"><h2 className="footer-link">Examples</h2></Link>
+        <div className="github-btn">
+          <GitHubButton href="https://github.com/sourabhdesai/PixelMist"
+            data-color-scheme="no-preference: dark; light: dark; dark: dark;"
+            data-icon="octicon-star" aria-label="Star sourabhdesai/PixelMist on GitHub">Star</GitHubButton>
         </div>
-      </Router>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactGA from 'react-ga';
+import {
+  BrowserRouter as Router,
+} from "react-router-dom";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -9,7 +12,9 @@ ReactGA.initialize('UA-68475136-3');
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
