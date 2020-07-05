00f149762dcfadb47267c15da1ab00020a93b50c
sourabhdesai <sourabhdesai@gmail.com>
2020-07-04 21:06:00 -0500

    Add Web Worker image calculation + Progress indicator

--- src/components/PixelCanvas.js
+++ src/components/PixelCanvas.js
@@ -1,43 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import _ from "lodash";
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import PixelGenerationWorker from 'workerize-loader!../utils/pixelgeneration.worker.js';
 import './PixelCanvas.css'
-import {BitMap} from 'glaciall-bitmap';
-import {ExpressionPixelGenerator} from '../utils/PixelGenerator';
 
 const DEFAULT_SCALE = 1000;
+const PROGRESS_UPDATE_INTERVAL = 5;
+const workerInstance = PixelGenerationWorker();
+let workerReady = false;
 
 
-/**
- * Taken from here: https://stackoverflow.com/a/8468879
- */
-function toHexValue(r, g, b) {
-    return (r << 16) | (g << 8) | b;
-}
+export default function PixelCanvas({ width, height, red, green, blue }) {
+    const [imageBase64Src, setImageBase64Src] = useState(null);
+    const [workerIsReady, setWorkerIsReady] = useState(workerReady);
+    const [renderedParams, setRenderedParams] = useState(null);
+    const [paramsInProgress, setParamsInProgress] = useState(null);
+    const [progress, setProgress] = useState(0.0);
 
-function updateCanvas(width, height, rGenerator, gGenerator, bGenerator) {
-    var bmp = new BitMap();
-    bmp.create(width, height, 0);
+    const paramsToRender = {
+        scale: DEFAULT_SCALE,
+        width, height, red, green, blue,
+    };
 
-    const rPixels = rGenerator.generatePixels(width, height, DEFAULT_SCALE).convertResultToRgbScale();
-    const gPixels = gGenerator.generatePixels(width, height, DEFAULT_SCALE).convertResultToRgbScale();
-    const bPixels = bGenerator.generatePixels(width, height, DEFAULT_SCALE).convertResultToRgbScale();
+    const paramsChanged = !_.isEqual(paramsToRender, renderedParams);
+    const currParamsAreInProgress = _.isEqual(paramsToRender, paramsInProgress);
 
-    for (let x = 0; x < width; x++) {
-        for (let y = 0; y < height; y++) {
-            const r = rPixels[x][y];
-            const g = gPixels[x][y];
-            const b = bPixels[x][y];
-            bmp.setPixel(x, y, toHexValue(r, g, b));
+    useEffect(() => {
+        workerInstance.onmessage = (message) => {
+            if (_.isEqual(message.data, { type: "RPC", method: "ready" })) {
+                setWorkerIsReady(true);
+                workerReady = true;
+            } else if (message.data.imageSource) {
+                setImageBase64Src(message.data.imageSource);
+                setRenderedParams(message.data.renderedParams);
+                setParamsInProgress(null);
+            } else if (message.data.totalProgress) {
+                const newProgress = Math.round(message.data.totalProgress * 100);
+                if ((newProgress % PROGRESS_UPDATE_INTERVAL) === 0) {
+                    setProgress(newProgress);
+                }
+            }
+        };
+
+        if (workerIsReady && paramsChanged && !currParamsAreInProgress) {
+            setParamsInProgress(paramsToRender);
+            workerInstance.calculateBase64(paramsToRender);
         }
+    });
+
+    if (_.isNil(imageBase64Src) || !workerIsReady || (paramsChanged && currParamsAreInProgress)) {
+        return <p>Loading {`${progress}%`}...</p>
+    } else {
+        return (<img width={`${width}px`} height={`${height}px`} src={imageBase64Src} className="main-canvas" />);
     }
-
-    return bmp.toBase64();
 }
-
-export default function PixelCanvas({width, height, red, green, blue}) {
-    const rGenerator = new ExpressionPixelGenerator(red);
-    const gGenerator = new ExpressionPixelGenerator(green);
-    const bGenerator = new ExpressionPixelGenerator(blue);
-    console.log({width, height, rGenerator, gGenerator, bGenerator});
-    const imageBase64Str = updateCanvas(width, height, rGenerator, gGenerator, bGenerator);
-    return (<img width={`${width}px`} height={`${height}px`} src={imageBase64Str} className="main-canvas"></img>);
-};
--- src/utils/pixelgeneration.worker.js
+++ src/utils/pixelgeneration.worker.js
@@ -1,4 +1,6 @@
+// src/worker.js
 import _ from "lodash";
+import { BitMap } from 'glaciall-bitmap';
 
 export function clamp(n, min, max) {
     return Math.min(Math.max(n, min), max);
@@ -37,12 +39,13 @@ export class PixelGeneratorResult {
 // Abstract class - do not instantiate. Implementors must override generate(x, y) method
 export class PixelGenerator {
 
-    generatePixels(widthPixels, heightPixels, scale) {
+    generatePixels(widthPixels, heightPixels, scale, progressCb) {
         scale = scale || 1;
         var min = Infinity;
         var max = -Infinity;
 
         const pixelGrid = _.times(widthPixels, x => {
+            progressCb(x / widthPixels);
             return _.times(heightPixels, yInverse => {
                 const y = heightPixels - 1 - yInverse;
                 const xInterp = linearInterpolate(x, 0, widthPixels, -scale, scale);
@@ -54,6 +57,7 @@ export class PixelGenerator {
                 return pixelVal;
             });
         });
+        progressCb(1.0);
 
         return new PixelGeneratorResult(pixelGrid, min, max);
     }
@@ -63,7 +67,7 @@ export class PixelGenerator {
     }
 }
 
-export class ExpressionPixelGenerator extends PixelGenerator {
+class ExpressionPixelGenerator extends PixelGenerator {
     constructor(expressionString) {
         super();
         this.expressionFunc = eval(`((x, y, w, h) => ${expressionString})`);
@@ -72,4 +76,55 @@ export class ExpressionPixelGenerator extends PixelGenerator {
     generate(xInput, yInput, width, height) {
         return this.expressionFunc(xInput, yInput, width, height);
     }
+}
+
+/**
+ * Taken from here: https://stackoverflow.com/a/8468879
+ */
+function toHexValue(r, g, b) {
+    return (r << 16) | (g << 8) | b;
+}
+
+function gridToBase64(width, height, rPixels, gPixels, bPixels) {
+    var bmp = new BitMap();
+    bmp.create(width, height, 0);
+
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            const r = rPixels[x][y];
+            const g = gPixels[x][y];
+            const b = bPixels[x][y];
+            bmp.setPixel(x, y, toHexValue(r, g, b));
+        }
+    }
+
+    return bmp.toBase64();
+}
+
+function rgbChannelProgressCbGenerator() {
+    let channelProgress = {};
+    return (channel) => {
+        channelProgress[channel] = channelProgress[channel] || 0.0;
+        return (progress) => {
+            channelProgress[channel] = progress;
+            const totalProgress = _.sum(_.values(channelProgress)) / 3.0;
+            postMessage({totalProgress});
+        };
+    };
+}
+
+export const calculateBase64 = ({ scale, width, height, red, green, blue }) => {
+    const progressCbGenerator = rgbChannelProgressCbGenerator();
+    const rPixels = new ExpressionPixelGenerator(red).generatePixels(width, height, scale, progressCbGenerator('r'))
+        .convertResultToRgbScale();
+    const gPixels = new ExpressionPixelGenerator(green).generatePixels(width, height, scale, progressCbGenerator('g'))
+        .convertResultToRgbScale();
+    const bPixels = new ExpressionPixelGenerator(blue).generatePixels(width, height, scale, progressCbGenerator('b'))
+        .convertResultToRgbScale();
+    const base64Str = gridToBase64(width, height, rPixels, gPixels, bPixels);
+
+    postMessage({
+        imageSource: base64Str,
+        renderedParams: { scale, width, height, red, green, blue },
+    });
 }
